28579c724ca4efa5354e38d3c60a057ca4d50b87
clinconao <cl.linconao@gmail.com>
2023-09-01 20:54:57 +1200

    modificación prompt

--- JS/index.js
+++ JS/index.js
@@ -14,9 +14,9 @@ const cantBoletas = parseInt(prompt("¿Cuántas boletas quieres calcular?"))
 const servicios = []
 
 for (let i = 0; i < cantBoletas; i++) {
-    const nombre = prompt("Ingresar nombre");
-    const servicio = prompt("Ingrese nombre del servicio realizado");
-    const monto = prompt("Ingrese monto a cobrar por el servicio")
+    const nombre = prompt("Ingresar tu nombre");
+    const servicio = prompt("Ingresa el tipo de servicio realizado");
+    const monto = prompt("Ingresa el monto a cobrar por el servicio")
     const tasaRetencion = prompt("Ingresa tu tasa de retención (solo numeros enteros)")
     const anio = prompt("Ingresa el año en que realizaste el servicio")
     const calculo = monto / 100 * tasaRetencion
